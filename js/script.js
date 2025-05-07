1cb474e627d3b1c9172922f29f364054848e4062
iagoliziero <iagolizierox14@gmail.com>
2025-05-07 16:07:50 -0300

    feat: Insert cell validation

--- js/script.js
+++ js/script.js
@@ -13,26 +13,51 @@ popupButton.addEventListener('click', () => {
 
 const btnRouter = document.querySelector('#button__router')
 
+//VALIDATION CELL
+
+const validCellRegex = /^\(?\d{2}\)?\s?\d{4,5}-?\d{4}$/
+
+const validCell = (cell) => {
+    const containerError = document.querySelector('.container__error__cell');
+    containerError.innerHTML = '';
+
+    if(!validCellRegex.test(cell)) {
+        const paragraph = document.createElement('p');
+        paragraph.setAttribute('class', 'error__p');
+        paragraph.innerHTML = 'Erro ao validar o telefone.'
+        containerError.appendChild(paragraph);
+        return false;
+    } else {
+        return true;
+    }
+}
+
+
 btnRouter.addEventListener('click', async () => {
     const cpfInput = document.querySelector('#form__cpf').value;
     const cpf = cpfInput.replace(/\D/g, '')
+    //VALIDATION CPF
     const BASE_URL = `https://api-cpf.vercel.app/cpf/valid/${cpf}`;
-    
+    const cellInput = document.querySelector('#form__cell').value;
+    const containerError = document.querySelector('.container__error');
+    containerError.innerHTML = '';
     try {
         const response = await fetch(BASE_URL, {method: 'GET'})
         if(response.status === 200) {
             const data = await response.json();
-            if(data.Valid) {
+            if(data.Valid && validCell(cellInput)) {
                 window.location = '../index.html';
             } else if (data.Valid == false && data != 13 && typeof data != 'string' ) {
-                const containerError = document.querySelector('.container__error');
                 const paragraph = document.createElement('p');
                 paragraph.setAttribute('class', 'error__p');
                 paragraph.innerHTML = 'Erro ao validar o CPF. '
                 containerError.appendChild(paragraph);
-            }
+            } 
         }
     } catch (error) {
         console.log(`Erro ao pegar o cpf. ${error}`);
     }   
+    
 })
+
+
