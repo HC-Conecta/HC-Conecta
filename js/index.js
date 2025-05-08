2617e69622327d22fb3db176bcaaceadca39972b
iagoliziero <iagolizierox14@gmail.com>
2025-05-08 12:44:46 -0300

    feat: add click event to section on index page

---
+++ js/index.js
@@ -0,0 +1,29 @@
+//ROUTER INDEX
+
+const sectionCalendar = document.getElementById('section-div__appointment');
+const routerCalendar = document.getElementById('router-appointment');
+
+sectionCalendar.addEventListener('click', () => {
+    routerCalendar.click();
+})
+
+const sectionDoubt = document.getElementById('section-doubt');
+const routerDoubt = document.getElementById('router-doubt');
+
+sectionDoubt.addEventListener('click', () => {
+    routerDoubt.click();
+})
+
+const sectionSuport = document.getElementById('section-suport');
+const routerSuport = document.getElementById('router-suport');
+
+sectionSuport.addEventListener('click', () => {
+    routerSuport.click();
+})
+
+const sectionAboutUs = document.getElementById('section-aboutUs');
+const routerAboutUs = document.getElementById('router-aboutUs');
+
+sectionAboutUs.addEventListener('click', () => {
+    routerAboutUs.click();
+})
