0dd2d8e7616e2b8090e8b600946de62f1745d78c
iagoliziero <iagolizierox14@gmail.com>
2025-05-13 08:26:13 -0300

    style: insert responsiveness in appointment page

--- js/appointment.js
+++ js/appointment.js
@@ -3,7 +3,6 @@ const accordions = document.querySelectorAll(".accordion");
 accordions.forEach(accordion => {
     const header = accordion.querySelector('.accordion-header');
     const body = accordion.querySelector('.accordion-body');
-
     const accordionImg = accordion.querySelector('.accordion-img');
 
     header.addEventListener('click', () => {
