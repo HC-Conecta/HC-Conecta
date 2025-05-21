5177fbad3c367c1e2a5d9e616d858dc9e5e46d0f
iagoliziero <iagolizierox14@gmail.com>
2025-05-20 23:11:08 -0300

    feat: inserting navigation for discover page

---
+++ js/events/navigation-login.js
@@ -0,0 +1,8 @@
+const sectionDiscover = document.getElementById('section-discover');
+const navigateDiscover = document.querySelector('.navigation-discover');
+
+sectionDiscover.addEventListener('click', () => {
+    navigateDiscover.click();
+})
+
+const navigateGuide = document.querySelector('')
